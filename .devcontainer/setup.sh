2913e95d6bdcc6b5fe35e6be07bc78db6e41e946
Wu Huijie <hjwu85@gmail.com>
2023-09-16 10:04:06 +0000

    Initial commit

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NODE_VERSION="20"
+
+# install curl, git, ...
+apt-get update
+apt-get install -y curl git jq make
+
+useradd -m user
+su user
+
+# Install Node version manager to install different versions of Node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+
+# Check Node version
+nvm install ${NODE_VERSION}
+nvm alias default ${NODE_VERSION}
+nvm use ${NODE_VERSION}
+node -v
+
+# Install bun
+curl -fsSL https://bun.sh/install | bash
