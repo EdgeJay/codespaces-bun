dae8f367bf99121a55b0332269ff56b979acf443
Wu Huijie <hjwu85@gmail.com>
2023-09-16 10:14:33 +0000

    add unzip, add command to reload bashrc

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,13 +4,14 @@ NODE_VERSION="20"
 
 # install curl, git, ...
 apt-get update
-apt-get install -y curl git jq make
+apt-get install -y unzip curl git jq make
 
 useradd -m user
 su user
 
 # Install Node version manager to install different versions of Node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+source ~/.bashrc 
 
 # Check Node version
 nvm install ${NODE_VERSION}
